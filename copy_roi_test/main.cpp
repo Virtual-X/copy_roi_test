3d901cf5637a74f9be553081c3d004a3525186a2
Igor Beati <heartwork52@yahoo.com>
2017-03-04 13:51:52 +0100

    Initial Commit

---
+++ copy_roi_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  copy_roi_test
+//
+//  Created by Igor Beati on 04.03.17.
+//  Copyright © 2017 Igor Beati. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
